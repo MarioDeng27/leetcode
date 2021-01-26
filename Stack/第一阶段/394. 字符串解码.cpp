b523dfd8d89a8081ea4f541fd89152b6c7287a00
Mario <941971066@qq.com>
2021-01-27 02:11:03 +0800

    20210127-v1

--- Stack/第一阶段/394. 字符串解码.cpp
+++ Stack/第一阶段/394. 字符串解码.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <map>
 #include <cmath>
+#include <iostream>
+#include <map>
 #include <set>
-#include <unordered_map>
 #include <stack>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 /*
     自己的想法：用栈实现的
@@ -59,5 +59,52 @@ public:
             st.pop();
         }
         return sa;
+    }
+};
+/*
+    一个数子栈一个字符串栈
+    一旦出现'['就将之前保存好的数子n 和 字符串word 保存到各自的栈中，注意，n和word不是对应的关系，本次的n应该与下一次的word对应
+    一旦出现']'就将之前在各自栈中保存好的栈顶元素数字 k 和 temp取出，让 temp + 3*word(当前word),然后将word = 该值
+*/
+class Solution
+{
+public:
+    string decodeString(string s)
+    {
+        stack<int> stnum;
+        stack<string> ststr;
+        int n = 0;
+        string word;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (isdigit(s[i]))
+            {
+                n = n * 10 + (s[i] - '0');
+            }
+            else if (s[i] == '[')
+            {
+                ststr.push(word);
+                stnum.push(n);
+                word = "";
+                n = 0;
+            }
+            else if (isalpha(s[i]))
+            {
+                word += s[i];
+            }
+            else // c == ']'
+            {
+                string temp = ststr.top();
+                ststr.pop();
+                int k = stnum.top();
+                stnum.pop();
+                while (k--)
+                {
+                    temp += word;
+                }
+                word = temp;
+            }
+        }
+        return word;
     }
 };
