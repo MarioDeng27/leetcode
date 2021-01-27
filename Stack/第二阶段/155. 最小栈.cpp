0d7352119caa6beb33546bd2709a529552fb5bdc
Mario <941971066@qq.com>
2021-01-28 01:46:01 +0800

    20210128-v1

--- Stack/第二阶段/155. 最小栈.cpp
+++ Stack/第二阶段/155. 最小栈.cpp
@@ -1,3 +1,13 @@
+/*
+ * @FilePath: \Sorte:\DataStructures-Algorithms\DataStructures-Algorithms\leetcode\Stack\第二阶段\155. 最小栈.cpp
+ * @Brief: 
+ * @Version: 1.0
+ * @Date: 2020-11-14 21:06:25
+ * @Author: Mario Deng
+ * @Copyright: your copyright description
+ * @LastEditors: Mario Deng
+ * @LastEditTime: 2021-01-28 01:45:40
+ */
 
 class MinStack
 {
@@ -30,5 +40,41 @@ public:
     int getMin()
     {
         return min_vec.back();
+    }
+};
+
+class MinStack
+{
+public:
+    /** initialize your data structure here. */
+    stack<int> stval;
+    stack<int> stmin;
+    MinStack()
+    {
+        stmin.push(INT_MAX);
+    }
+
+    void push(int x)
+    {
+        //保证在每个不同的栈顶元素有不同的栈元素最小值
+        stval.push(x);
+        //如果stmin.top()>x,则表示，当前x是整个栈中的最小值，若反之，则表示，之前前一个的stmin的top才是当前最小值
+        stmin.push(min(stmin.top(), x));
+    }
+
+    void pop()
+    {
+        stval.pop();
+        stmin.pop();
+    }
+
+    int top()
+    {
+        return stval.top();
+    }
+
+    int getMin()
+    {
+        return stmin.top();
     }
 };
