a1ed79ecf60e97612aa8c19a28f3654a27c9c771
mario <941971066@qq.com>
2021-09-19 00:24:44 +0800

    20210919-v1

---
+++ 框架模板/数组中和为K的所有子集可能性，每个元素只能使用一次.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Mario Deng
+ * @Date: 2021-09-18 22:18:09
+ * @LastEditors: Mario Deng
+ * @LastEditTime: 2021-09-18 22:18:09
+ */
+//回溯
+vector<vector<int>> vec;
+void backtrack(vector<int> &nums, int C, int index, vector<int> track, int sum)
+{
+    if (index == nums.size())
+        return;
+
+    sum += nums[index];
+    track.push_back(index);
+    if (sum == C)
+    {
+        vec.push_back(track);
+    }
+    backtrack(nums, C, index + 1, track, sum);
+    track.pop_back();
+    sum -= nums[index];
+
+    backtrack(nums, C, index + 1, track, sum);
+}
