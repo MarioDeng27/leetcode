c21da94f839178a132d142c56e11b4c8c8bf7a59
mario <941971066@qq.com>
2022-07-18 12:48:58 +0800

    20220718-v1

--- leetcode101/动态规划/542. 01 矩阵(好题目，不用BFS，而是用两次dp来进行遍历搜索).cpp
+++ leetcode101/动态规划/542. 01 矩阵(好题目，不用BFS，而是用两次dp来进行遍历搜索).cpp
@@ -2,53 +2,58 @@
  * @Description:
  * @Version: 1.0
  * @Autor: Mario Deng
- * @Date: 2022-07-12 13:54:48
+ * @Date: 2022-07-18 11:37:54
  * @LastEditors: Mario Deng
- * @LastEditTime: 2022-07-18 11:41:09
+ * @LastEditTime: 2022-07-18 12:48:42
  */
-#include <algorithm>
-#include <array>
-#include <atomic>
-#include <bits/stdc++.h>
-#include <cctype>
-#include <cmath>
-#include <condition_variable>
-#include <functional>
-#include <iostream>
-#include <list>
-#include <map>
-#include <memory>
-#include <mutex>
-#include <pthread.h> // pthread_create(), pthread_join()
-#include <queue>
-#include <semaphore.h> // sem_init()
-#include <set>
-#include <sstream>
-#include <stack>
-#include <thread>
-#include <unordered_map>
-#include <vector>
-using namespace std;
-
-/* #include <bits/stdc++.h>
-using namespace std; */
-
-struct ListNode
+class Solution
 {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
+    {
+        int m = mat.size();
+        int n = m ? mat[0].size() : 0;
+        if (!m && !n)
+            return {};
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (mat[i][j] == 0)
+                    continue;
+                if (i == 0)
+                {
+                    dp[i][j] = j > 0 ? dp[i][j - 1] + 1 : (m + n);
+                }
+                else
+                {
+                    dp[i][j] = j > 0 ? min(dp[i - 1][j], dp[i][j - 1]) + 1 : dp[i - 1][j] + 1;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (mat[i][j] == 0)
+                    continue;
+                if (i == m - 1)
+                {
+                    int t = j < n - 1 ? dp[i][j + 1] + 1 : (m + n);
+                    dp[i][j] = min(dp[i][j], t);
+                }
+                else
+                {
+                    int t = j < n - 1 ? min(dp[i][j + 1], dp[i + 1][j]) + 1 : dp[i + 1][j] + 1;
+                    dp[i][j] = min(dp[i][j], t);
+                }
+            }
+        }
+        return dp;
+    }
 };
-
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+//更加自如
 class Solution
 {
 public:
@@ -100,8 +105,3 @@ public:
         return dp;
     }
 };
-int main()
-{
-    Derived d;
-    return 0;
-}
